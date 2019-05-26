ada7b3e479363289f95e08929a56edf62b78498d
Yash <yash@Yashs-MacBook-Pro.local>
2019-05-26 21:01:55 +0530

    Adding login and signup screens [only UI]

--- app/src/main/java/com/binc/nutrilist/SigninActivity.kt
+++ app/src/main/java/com/binc/nutrilist/SigninActivity.kt
@@ -1,12 +1,19 @@
 package com.binc.nutrilist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SigninActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+    }
+
+    fun startSignupActivity(view: View) {
+        val signupActivity = Intent(this@SigninActivity, SignupActivity::class.java)
+        startActivity(signupActivity)
     }
 }
